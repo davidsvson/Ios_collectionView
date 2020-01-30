6e9dfc70c0e6dced2fb3efd0d6f9ea04213a5103
David Svensson <davidsvson@gmail.com>
2020-01-30 14:51:52 +0100

    1.0

---
+++ CollectionTemp/CollectionViewCell.swift
@@ -0,0 +1,20 @@
+//
+//  CollectionViewCell.swift
+//  CollectionTemp
+//
+//  Created by David Svensson on 2020-01-30.
+//  Copyright © 2020 David Svensson. All rights reserved.
+//
+
+import UIKit
+
+class CollectionViewCell: UICollectionViewCell {
+
+    @IBOutlet weak var label: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
--- CollectionTemp/ViewController.swift
+++ CollectionTemp/ViewController.swift
@@ -8,13 +8,38 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController , UICollectionViewDataSource {
+    
+    let cellId = "CellId"
+    
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        let nib = UINib(nibName: "CollectionViewCell", bundle: nil)
+        
+        collectionView.register(nib, forCellWithReuseIdentifier: cellId)
+        
+        collectionView.dataSource = self
+        
     }
-
-
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellId, for: indexPath) as! CollectionViewCell
+        
+        cell.label.text = "David"
+        
+        return cell
+    }
+    
+    
 }
 
